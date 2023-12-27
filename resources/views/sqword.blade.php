94f637b662852d019608baaa11b567fbeadb6e8f
cnievaslozano <cnievaslozano@gmail.com>
2023-12-27 19:44:01 +0100

    sqword

---
+++ resources/views/sqword.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="cat">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="/favicon.svg" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="Make words - compete with friends." />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
+    <link rel="manifest" href="/manifest.json" />
+    <title>Sqword</title>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DJM6J1D7T7"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [],
+        window.gtag = function() {
+            dataLayer.push(arguments)
+        },
+        window.gtag("js", new Date),
+        window.gtag("config", "G-DJM6J1D7T7")
+    </script>
+    <script defer="defer" src="{{ asset('js/main.905598ef.js') }}"></script>
+    <link href="{{ asset('css/main.c2ff7bda.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <noscript>Necesitas Javascript para ejecutar esta página.</noscript>
+    <div id="root"></div>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/sqword', function () {
+    return view('sqword');
+});
